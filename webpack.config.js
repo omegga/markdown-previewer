e126e0bcd5cb4abf76736ef7f9a46e7bfd002b82
omegga <whymagnet@gmail.com>
2019-05-08 02:09:56 +0200

    resolve js and jxs extensions in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,9 @@ module.exports = {
     path: path.join(__dirname, "dist"),
     filename: "bundle.js"
   },
+  resolve: {
+    extensions: [ ".js", ".jsx" ]
+  },
   module: {
     rules: [
       {
